0b250222475a0e8681393319c02c7ee1e2ecbf63
mattdesl <dave.des@gmail.com>
2015-07-12 14:33:06 -0400

    change way event sending works

--- send.js
+++ send.js
@@ -16,13 +16,14 @@ var command = argv._[0]
 var modules = argv._.slice(1)
 var save = ''
 
-if (!modules) {
+if (modules.length === 0) {
   // bail if we are installing a project
   process.exit(0)
 }
 
 var conf = require('rc')('npm-install-analytics')
 var ua = require('universal-analytics')
+var chalk = require('chalk')
 
 // normalize shorthands
 if (command === 'i') {
@@ -42,12 +43,15 @@ if (argv.save) {
 }
 
 if (!conf.id) {
-  var chalk = require('chalk')
   console.error(chalk.yellow('npm-analytics'), 'could not find tracking ID')
   process.exit(0)
 }
 
 var visitor = ua(conf.id)
-modules.forEach(function (module) {
-  visitor.event(command, save, module)
+modules.forEach(function (module) {  
+  visitor.event(command, module, save, function (err) {
+    if (err) {
+      console.error(chalk.red('npm-analytics'), err.message)
+    }
+  })
 })
