dc4df84ac27ef09b6d82af2d234df57014757ad1
mattdesl <dave.des@gmail.com>
2015-07-12 14:17:05 -0400

    bail when installing source

--- send.js
+++ send.js
@@ -12,11 +12,17 @@ var argv = require('minimist')(process.argv.slice(2), {
   boolean: ['save', 'save-dev', 'save-exact', 'save-optional']
 })
 
-var conf = require('rc')('npm-install-analytics')
-var ua = require('universal-analytics')
 var command = argv._[0]
 var modules = argv._.slice(1)
 var save = ''
+
+if (!modules) {
+  // bail if we are installing a project
+  process.exit(0)
+}
+
+var conf = require('rc')('npm-install-analytics')
+var ua = require('universal-analytics')
 
 // normalize shorthands
 if (command === 'i') {
